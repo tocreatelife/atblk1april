22b2f0caba9fa08d1addc2b830ce0f3ce5facb8c
Dimitriy <tocreatelife@ya.ru>
2025-03-30 18:41:32 +0300

    Delete qr7.js

--- qr7.js
+++
@@ -1,5 +0,0 @@
-// Автоматически запускаем видео при загрузке страницы
-window.onload = function () {
-  const iframe = document.querySelector("iframe");
-  iframe.src = iframe.src; // Перезагружаем iframe для автовоспроизведения
-};
